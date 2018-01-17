36fe0e953202f7ba03e2ca6ff66c47deca38e6d9
Beau Russell <beaur97@att.net>
2018-01-17 10:17:42 -0600

    Solution to Compile Error
    Corrected the extension of the component class, and fixed where the imports of <BusinessList /> and <SearchBar /> pointed to

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import BusinessList from './components/BusinessList';
-import SearchBar from './components/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
