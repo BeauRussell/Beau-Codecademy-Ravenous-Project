ac0494a127d3f9cdae42038bc115bfc3e9e77a5c
Beau Russell <beaur97@att.net>
2018-01-20 21:26:27 -0600

    Fixed SortByOptions Not Appearing
    Moved everything into the class to prevent left out information, like a SortBy list being prevented from appearing

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-	'Best Match': 'best_match',
-	'Highest Rated': 'rating',
-	'Most Reviewed': 'review_count'
-};
-
-const getSortByClass = function(sortByOption) {
-	if (this.state.sortBy === sortByOption) {
-		return 'active';
-	} else {
-		return '';
-	}
-}
-
 
 class SearchBar extends React.Component {
 	constructor(props) {
@@ -22,11 +8,25 @@ class SearchBar extends React.Component {
 		this.handleTermChange = this.handleTermChange.bind(this);
 		this.handleLocationChange = this.handleLocationChange.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
+		this.handleSortByChange = this.handleSortByChange.bind(this);
 		this.state = {
 			term: '',
 			location: '',
 			sortBy: 'best_match'
 		};
+		this.sortByOptions = {
+			'Best Match': 'best_match',
+			'Highest Rated': 'rating',
+			'Most Reviewed': 'review_count'
+		};
+	}
+
+	getSortByClass(sortByOption) {
+		if (this.state.sortBy === sortByOption) {
+			return 'active';
+		} else {
+			return '';
+		}
 	}
 
 	handleSortByChange (sortByOption) {
@@ -47,18 +47,22 @@ class SearchBar extends React.Component {
 	}
 
 	renderSortByOptions() {
-		return Object.keys(sortByOptions).map(sortByOption => {
-			let sortByOptionValue = sortByOptions[sortByOption];
-			<li className={getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>{sortByOption}</li>
-		});
-	}
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+                  key={sortByOptionValue}
+                  onClick={this.handleSortByChange}>
+                {sortByOption}
+             </li>);
+    });
+  }
 
 	render() {
 		return (
 			<div className="SearchBar">
   				<div className="SearchBar-sort-options">
     				<ul>
-      					{this.renderSortByOptions}
+      					{this.renderSortByOptions()}
     				</ul>
   				</div>
   				<div className="SearchBar-fields">
